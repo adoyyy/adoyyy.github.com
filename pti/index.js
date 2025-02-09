871d034b4137e7c00a3a85c0bf48b7a8359564c3
Adoyyy <125505293+adoyyy@users.noreply.github.com>
2025-02-10 05:30:15 +0700

    Update index.js
    fix modal

--- pti/index.js
+++ pti/index.js
@@ -3,37 +3,36 @@ const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
-// event listeners
+// Event listeners
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
     mealDetailsContent.parentElement.classList.remove('showRecipe');
 });
 
-
-// get meal list that matches with the ingredients
-function getMealList(){
+// Get meal list that matches with the ingredients
+function getMealList() {
     let searchInputTxt = document.getElementById('search-input').value.trim();
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
     .then(response => response.json())
     .then(data => {
         let html = "";
-        if(data.meals){
+        if (data.meals) {
             data.meals.forEach(meal => {
                 html += `
-                    <div class = "meal-item" data-id = "${meal.idMeal}">
-                        <div class = "meal-img">
-                            <img src = "${meal.strMealThumb}" alt = "food">
+                    <div class="meal-item" data-id="${meal.idMeal}">
+                        <div class="meal-img">
+                            <img src="${meal.strMealThumb}" alt="food">
                         </div>
-                        <div class = "meal-name">
+                        <div class="meal-name">
                             <h3>${meal.strMeal}</h3>
-                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                            <a href="#" class="recipe-btn">Get Recipe</a>
                         </div>
                     </div>
                 `;
             });
             mealList.classList.remove('notFound');
-        } else{
+        } else {
             html = "Sorry, we didn't find any meal!";
             mealList.classList.add('notFound');
         }
@@ -42,19 +41,18 @@ function getMealList(){
     });
 }
 
-
-// get recipe of the meal
-function getMealRecipe(e){
+// Get recipe of the meal
+function getMealRecipe(e) {
     e.preventDefault();
-    if(e.target.classList.contains('recipe-btn')){
-        let mealItem = e.target.parentElement.parentElement;
+    if (e.target.classList.contains('recipe-btn')) {
+        let mealItem = e.target.parentElement .parentElement;
         fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
         .then(response => response.json())
         .then(data => mealRecipeModal(data.meals));
     }
 }
 
-// create a modal
+// Create a modal
 function mealRecipeModal(meal) {
     meal = meal[0];
     let html = `
